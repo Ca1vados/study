d301d8fbb1f8d70753b5ca57a52e9ff8f8cef85a
Alekseev Pavel <c4lvados@yandex.ru>
2024-09-30 20:51:59 +0300

    MyEncyclopedia and begin LoginPassTask

--- 10LoginPassword/main.go
+++ 10LoginPassword/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+type User struct {
+	Login    string `yaml:"Id"`
+	Pass     string `json:"Pass"`
+	PassHash string `json:""`
+	Secret   string `json:"Secret"`
+}
+
+func main() {
+	var UserInput User
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Введите логин:")
+	Login, _ := reader.ReadString('\n')
+	UserInput.Login = Login
+	fmt.Println("Введите пароль:")
+	Password, _ := reader.ReadString('\n')
+	UserInput.Pass = Password
+	fmt.Printf("Структура пользователя: %+v\n", UserInput)
 }
