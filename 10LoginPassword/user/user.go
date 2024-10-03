8437be72c047e9bef7480a6b94113f8d2bef26a1
Alekseev Pavel <c4lvados@yandex.ru>
2024-10-03 14:48:43 +0300

    rename 1-9 task folders

--- 10LoginPassword/user/user.go
+++ 10LoginPassword/user/user.go
@@ -5,3 +5,12 @@ type User struct {
 	PassHash string `json:""`
 	Secret   string `json:"Secret"`
 }
+
+/*
+будет принимать введенные от пользователя данные,
+сверять их с данными полученными функцией Database
+и при удачной верификации возвращать дополненную полем секрет структуру
+*/
+func Veryfication(a User) User {
+
+}
