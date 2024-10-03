fcc906548fc69e951f35d1772b12ae111615c174
Alekseev Pavel <c4lvados@yandex.ru>
2024-10-03 12:30:59 +0300

    task12 doubleArray

---
+++ 12ChangeArrayElements/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func doubleArray(arr *[5]int) *[5]int {
+	for a, _ := range arr {
+		arr[a] *= 2
+	}
+	return arr
+}
+func main() {
+	numbers := [5]int{1, 2, 3, 4, 5}
+	doubleArray(&numbers)
+	fmt.Println(numbers)
+}
