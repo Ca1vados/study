8c448b3de8d9dd0c7a32bd8922f5549e71756490
Alekseev Pavel <c4lvados@yandex.ru>
2024-10-14 21:23:30 +0300

    smtshit

--- 10LoginPassword/tests/database_test.go
+++ 10LoginPassword/tests/database_test.go
@@ -1,6 +1,11 @@
 package tests
 
-import "testing"
+import (
+	"LoginPass/database"
+	"log"
+	"os"
+	"testing"
+)
 
 /*
 тест на чтение json файла.
@@ -11,6 +16,25 @@ import "testing"
 */
 
 func TestReadJsonFile(t *testing.T) {
-	var NewUser = User{Login: user3, PassHash: somePass, Secret: someSecret}
+	filename := "TestReadJsonFile.json"
+	expString := "1"
+	res := 1
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) // флаги, разрешения, которые мы даем файлу
+	if err != nil {
+		log.Fatalf("Ошибка при открытии файла: %v", err)
+	}
+
+	// Запись строки в файл
+	if _, err := file.WriteString(expString); err != nil {
+		log.Fatalf("Ошибка при записи в файл: %v", err)
+	}
+	file.Close()
+	data, err := database.ReadJsonFile(filename)
+	if err != nil {
+		t.Errorf("func ReadJsonFile return error")
+	}
+	if data != res {
+		t.Errorf("data = %v, expected %v", data, res)
+	}
 
 }
