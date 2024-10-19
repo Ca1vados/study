b4f6d04bf233701ddf6e57b2b5e9ab9b566c5d7e
Alekseev Pavel <c4lvados@yandex.ru>
2024-10-19 15:39:52 +0300

    bubble and quick sorts complete

--- 13Sort/main.go
+++ 13Sort/main.go
@@ -6,7 +6,18 @@ import (
 )
 
 func main() {
-	arr := []int{5, 2, 9, 1, 5, 6}
-	sortedArr := sortfuncs.InsertionSort(arr)
-	fmt.Println(sortedArr)
+	arrForInsertionSort := []int{5, 2, 9, 1, 5, 6}
+	fmt.Printf("Массив для сортировки простыми вставками: %v\n", arrForInsertionSort)
+	sortedArrInsertionSort := sortfuncs.InsertionSort(arrForInsertionSort)
+	fmt.Printf("Отсортированный простыми вставками массив: %v\n", sortedArrInsertionSort)
+
+	arrForBubbleSort := []int{4, 5, 3, 1, 2, 6, 8, 10}
+	fmt.Printf("Массив для сортировки пузырьками: %v\n", arrForBubbleSort)
+	sortedArrForBubbleSort := sortfuncs.BubbleSort(arrForBubbleSort)
+	fmt.Printf("Отсортированный пузырьками массив: %v\n", sortedArrForBubbleSort)
+
+	arrForQuickSort := []int{7, 3, 1, 34, 12, 23, 4, 6}
+	fmt.Printf("Массив для быстрой сортировки: %v\n", arrForQuickSort)
+	sortedArrForQuickSort := sortfuncs.MyQuickSort(arrForQuickSort)
+	fmt.Printf("Отсортированный быстрой сортировкой массив: %v\n", sortedArrForQuickSort)
 }
--- 13Sort/main_test.go
+++ 13Sort/main_test.go
@@ -15,11 +15,33 @@ func TestInsertionSort(t *testing.T) {
 		{[]int{3, 3, 3}, []int{3, 3, 3}},
 		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}},
 		{[]int{5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5}},
+		{[]int{4, 2, 4, 54, 23, 12, 43, 54, 65, 76, 12, 433, 16, 11, 1, 87, 65, 43, 54, 32, 43, 12, 32, 54, 32, 12, 65, 32, 21, 54}, []int{1, 2, 4, 4, 11, 12, 12, 12, 12, 16, 21, 23, 32, 32, 32, 32, 43, 43, 43, 54, 54, 54, 54, 54, 65, 65, 65, 76, 87, 433}},
 		{[]int{}, []int{}},
 	}
 
 	for _, test := range tests {
 		result := sortfuncs.InsertionSort(test.input)
+		if !reflect.DeepEqual(result, test.expected) {
+			t.Errorf("For input %v, expected %v, but got %v", test.input, test.expected, result)
+		}
+	}
+}
+
+func TestBubbleSort(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected []int
+	}{
+		{[]int{5, 2, 9, 1, 5, 6}, []int{1, 2, 5, 5, 6, 9}},
+		{[]int{3, 3, 3}, []int{3, 3, 3}},
+		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}},
+		{[]int{5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5}},
+		{[]int{4, 2, 4, 54, 23, 12, 43, 54, 65, 76, 12, 433, 16, 11, 1, 87, 65, 43, 54, 32, 43, 12, 32, 54, 32, 12, 65, 32, 21, 54}, []int{1, 2, 4, 4, 11, 12, 12, 12, 12, 16, 21, 23, 32, 32, 32, 32, 43, 43, 43, 54, 54, 54, 54, 54, 65, 65, 65, 76, 87, 433}},
+		{[]int{}, []int{}},
+	}
+
+	for _, test := range tests {
+		result := sortfuncs.BubbleSort(test.input)
 		if !reflect.DeepEqual(result, test.expected) {
 			t.Errorf("For input %v, expected %v, but got %v", test.input, test.expected, result)
 		}
@@ -35,6 +57,7 @@ func TestQuickSort(t *testing.T) {
 		{[]int{3, 3, 3}, []int{3, 3, 3}},
 		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}},
 		{[]int{5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5}},
+		{[]int{4, 2, 4, 54, 23, 12, 43, 54, 65, 76, 12, 433, 16, 11, 1, 87, 65, 43, 54, 32, 43, 12, 32, 54, 32, 12, 65, 32, 21, 54}, []int{1, 2, 4, 4, 11, 12, 12, 12, 12, 16, 21, 23, 32, 32, 32, 32, 43, 43, 43, 54, 54, 54, 54, 54, 65, 65, 65, 76, 87, 433}},
 		{[]int{}, []int{}},
 	}
 
